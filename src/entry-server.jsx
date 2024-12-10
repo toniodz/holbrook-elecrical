cb3f47aef760c84cf9d14acdc88789940e8be721
toniodz <tony@dzines.co.uk>
2024-12-10 21:21:30 +0000

    yggy

--- src/entry-server.jsx
+++ src/entry-server.jsx
@@ -1,6 +1,14 @@
-import ReactDOMServer from 'react-dom/server'
-import { StaticRouter } from 'react-router-dom/server'
-import App from './App'
+import ReactDOMServer from 'react-dom/server';
+import { StaticRouter } from 'react-router-dom/server';
+import App from './App';
+
+const routes = [
+  '/',
+  '/services',
+  '/projects',
+  '/about',
+  '/contact'
+];
 
 export function render(url) {
   return ReactDOMServer.renderToString(
@@ -9,3 +17,5 @@ export function render(url) {
     </StaticRouter>
   )
 }
+
+export { routes };
